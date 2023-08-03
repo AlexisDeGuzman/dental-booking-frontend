1d921f0c9b35e58b19319c14ffacb9f8f404fa54
deguzmanalexisprexb <deguzmanalexisprexb@gmail.com>
2023-08-03 16:50:06 +0800

    Added logout component that clears the localStorage and navigate to login page

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,8 +1,24 @@
+import { Navigate } from 'react-router-dom';
+import UserContext from '../UserContext';
+import { useContext, useEffect } from 'react';
 
 const Logout = () => {
-    return (
-        <h1>Logout</h1>
-    );
+
+  const { unsetUser, setUser } = useContext(UserContext);
+
+  useEffect(() => {
+    // remove token from the localStorage
+    unsetUser();
+    // set user back to null
+    setUser({
+      id: null,
+      isAdmin: null
+    })
+  })
+
+  return (
+    <Navigate to = '/login'/>
+  );
 };
 
 export default Logout;
